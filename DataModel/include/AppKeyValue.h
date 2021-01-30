e8797f295cd2dcf4e7fe1800422df8ec0c130fe5
sriiixi <subratoroy@hotmail.com>
2021-01-30 22:32:09 +0530

    Cleanup

--- DataModel/include/AppKeyValue.h
+++
@@ -1,9 +0,0 @@
-#ifndef _APP_KEYVALUE
-#define _APP_KEYVALUE
-
-typedef struct AppKeyValue
-{
-	void* Dummy;
-}AppKeyValue;
-
-#endif
--- DataModel/include/Query.h
+++
@@ -1,9 +0,0 @@
-#ifndef _QUERY
-#define _QUERY
-
-typedef struct QueryStruct
-{
-	void* dummy;
-}QueryStruct;
-
-#endif
